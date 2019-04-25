eb9c24c663194c1c8886bb1069703214809b471a
Reno Sorensen <renojames19@gmail.com>
2019-04-25 00:58:11 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -35,19 +35,19 @@ def can_be_created_in_a_block(args = nil)
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.all.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.all.last
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.all.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.find_or_create_by(id: 1)
 end
 
 def can_find_by_multiple_attributes
